57e90adece19eaf7cd757bc0a125cdaf2dcfa047
Xu Xiaowei <xuxiaowei@lonwin.org>
2019-08-13 17:41:02 +0800

    begin 8.13

---
+++ PlotByCoordinate/PlotByCoordinate/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace PlotByCoordinate.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
