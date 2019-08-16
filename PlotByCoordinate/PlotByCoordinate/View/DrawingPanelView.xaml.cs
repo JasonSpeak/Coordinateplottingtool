5ffba1ca1b9d3aa38e441251a1bc5e37240892ea
Xu Xiaowei <xuxiaowei@lonwin.org>
2019-08-16 08:20:16 +0800

    paint

--- PlotByCoordinate/PlotByCoordinate/View/DrawingPanelView.xaml.cs
+++ PlotByCoordinate/PlotByCoordinate/View/DrawingPanelView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using PlotByCoordinate.ViewModel;
 namespace PlotByCoordinate.View
 {
     /// <summary>
@@ -20,34 +20,93 @@ namespace PlotByCoordinate.View
     /// </summary>
     public partial class DrawingPanelView : Window
     {
+        
+        
         public DrawingPanelView()
         {
 
             InitializeComponent();
-          
-
-        }
-    private void Window_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (e.LeftButton == MouseButtonState.Pressed)
-
-            {
-
-                this.DragMove();
+           
 
             }
+    
 
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+           
         }
 
-        private void Path_MouseMove(object sender, MouseEventArgs e)
+
+  
+
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
+            //var canvas = new Canvas();                              
+            
+        }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            Content = grid;
+            
+            var points =
+                new List<Point>()
+                {
+                    new Point(Convert.ToDouble((this.lx1.Text)), Convert.ToDouble((this.ly1.Text))),
+                    new Point(Convert.ToDouble((this.lx2.Text)), Convert.ToDouble((this.ly2.Text))),
+               //     new Point(10, 10)
+                };
+            var sb = new Storyboard();
+         
+            for (int i = 0; i < points.Count - 1; i++)
             {
-
-                
-
+                var lineGeometry = new LineGeometry(points[i], points[i]);
+                var path =
+                    new Path()
+                    {
+                        Stroke = Brushes.White,
+                        StrokeThickness = 3,
+                        Data = lineGeometry
+                    };
+                this.canvas.Children.Add(path);
+                var animation =
+                    new PointAnimation(points[i], points[i + 1], new Duration(TimeSpan.FromMilliseconds(1000)))
+                    {
+                        BeginTime = TimeSpan.FromMilliseconds(i * 1010)
+                    };
+                sb.Children.Add(animation);
+                RegisterName("geometry" + i, lineGeometry);
+                Storyboard.SetTargetName(animation, "geometry" + i);
+                Storyboard.SetTargetProperty(animation, new PropertyPath(LineGeometry.EndPointProperty));
+               
             }
+           
+            sb.Begin(this);
+            
+        }
+        static Point startPoint;
+        static Point endPoint;
+        static bool isCapture = false;
+        private void PathFigure_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Window win = sender as Window;
+            startPoint = e.GetPosition(win);
+            isCapture = true;
+        }
+
+        private void PathFigure_MouseMove(object sender, MouseEventArgs e)
+        {
+            endPoint = e.GetPosition(sender as Window);
+            if (isCapture)
+            {
+                DrawRectangle(sender); 
+            }
+        }
+        private void DrawRectangle(object sender)
+        {
+        
         }
     }
 }
--- PlotByCoordinate/PlotByCoordinate/ViewModel/CoordinateValueViewModel.cs
+++ PlotByCoordinate/PlotByCoordinate/ViewModel/CoordinateValueViewModel.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using PlotByCoordinate.Model;
+using GalaSoft.MvvmLight.Command;
+using System.Windows.Media.Animation;
+using System.Windows;
+using System.Windows.Media;
+
 namespace PlotByCoordinate.ViewModel
 {
     public class CoordinateValueViewModel:ViewModelBase
@@ -23,6 +28,26 @@ namespace PlotByCoordinate.ViewModel
             set { _coordinateValue = value; RaisePropertyChanged(() => CoordinateValue); }
         }
 
+        private RelayCommand beginPaint;
+
+        public RelayCommand BeginPaint
+        {
+            get
+            {
+                if (beginPaint == null) return new RelayCommand(() => ExecuteToPaint(), CanExcute);
+                return beginPaint;
+            }
+            set { beginPaint = value; }
+        }
+        private void ExecuteToPaint()
+        {
+
+            
+        }
+        public bool CanExcute()
+        {
+            return true;
+        }
 
     }
 }
